3bec6da4ed04068097e5525d5b2e327980adbc4f
Metin-3 <7kp0v0e@code.edu.az>
2025-01-09 17:49:59 +0400

    environments

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
+const config = require("../config/index");
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Metin' });
+router.get('/', function (req, res, next) {
+  res.render('index', { title: 'Metin', config });
 });
 
 module.exports = router;
